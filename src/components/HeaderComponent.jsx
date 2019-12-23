bb4a018f605c9d94b2b45983eb051ac05d32901d
sudobrackets <sudobrackets@gmail.com>
2019-12-24 01:20:46 +0530

    header component changes

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -5,6 +5,18 @@ class HeaderComponent extends Component {
         return (
             <div className="header-base">
                 <div>holiday house</div>
+                <div className="menus">
+                    <div>Shortlist</div>
+                    <div>List my house</div>
+                    <div>Help</div>
+                    <div>Login</div>
+                </div>
+                <div className="menu-base">
+                    <i class="fa fa-bars"></i>
+                    <div className="dropdown-content">
+
+                    </div>
+                </div>
             </div>
         )
     }
