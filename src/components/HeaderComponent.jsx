0c0246b32d0ed7b97a77a06f6983b3c7941c30a7
sudobrackets <sudobrackets@gmail.com>
2019-12-25 02:03:52 +0530

    header changes

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -1,21 +1,55 @@
 import React, { Component } from 'react'
 
 class HeaderComponent extends Component {
-    render() {
-        return (
-            <div className="header-base">
-                <div>holiday house</div>
-                <div className="menus">
-                    <div>Shortlist</div>
-                    <div>List my house</div>
-                    <div>Help</div>
-                    <div>Login</div>
-                </div>
-                <div className="menu-base">
-                    <i class="fa fa-bars"></i>
-                    <div className="dropdown-content">
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            menuList: ["Shortlist", "List my house", "Help", "Login"],
+            showMenu: false
+        }
+    }
+
+    onClickMenu = () => {
+        this.setState({ showMenu: true })
+    }
+
+    onClickCloseMenu = () => {
+        this.setState({ showMenu: false })
+    }
+
+    render() {
+        const { menuList, showMenu } = this.state;
+
+        return (
+            <div className="headerComp">
+                <div className="header-base">
+                    <div>holiday house</div>
+                    <div className="lg-view-flex menus">
+                        {menuList.map((item, index) => {
+                            return (
+                                <div>{item}</div>
+                            )
+                        })}
                     </div>
+                    <div className="sm-view menu-icon-div">
+                        {
+                            showMenu ?
+                                <i class="fa fa-bars" onClick={this.onClickCloseMenu}>B</i>
+                                :
+                                <i class="fa fa-bars" onClick={this.onClickMenu}>A</i>
+
+                        }
+                        <i class="fa fa-bars"></i>
+                    </div>
+                </div>
+                <div className={`dropdown-content ${showMenu ? 'show' : 'hide'}`}>
+                    {menuList.map((item, index) => {
+                        return (
+                            <div className="menu-item">{item}</div>
+                        )
+                    })}
                 </div>
             </div>
         )
