befdc94603bddddf8398986f798acee054839ac3
Nishanth <technobaskets@gmail.com>
2020-02-05 00:52:20 +0530

    map component changes

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
-import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
 
 import Header from './HeaderComponent'
+import HouseCard from './HouseCardComponent'
 
 const mapStyles = {
     width: '100%',
@@ -22,16 +23,40 @@ class MapComponent extends Component {
                 { latitude: 9.939093, longitude: 78.121719, name: "Maduari" },
                 { latitude: 10.8155, longitude: 78.6965, name: "Tiruchirapalli" },
                 { latitude: 13.067439, longitude: 80.237617, name: "Chennai" },
-            ]
+            ],
+            activeStore: {},
+            showingInfoWindow: true,
+            selectedPlace: {}
         }
     }
 
-    onClickCity = (city) => {
-        console.log('city details', city)
+    onMapClicked = () => {
+        console.log('onMapClicked')
+        if (this.state.showingInfoWindow) {
+            this.setState({
+                showingInfoWindow: false,
+                activeStore: null
+            })
+        }
     }
 
     centerMoved = (data) => {
         console.log('centerMoved', data)
+    }
+
+    onMouseoverCity = (index) => {
+        console.log('onMouseoverCity', index)
+    }
+
+    onClickCity = (props, marker, e) => {
+        console.log(props)
+        console.log(marker)
+        console.log(e)
+        this.setState({
+            selectedPlace: props,
+            activeStore: marker,
+            showingInfoWindow: true
+        });
     }
 
     displayMarkers = () => {
@@ -40,7 +65,11 @@ class MapComponent extends Component {
                 lat: city.latitude,
                 lng: city.longitude
             }}
-                onClick={() => this.onClickCity(city)} />
+                name={city.name}
+                onClick={this.onClickCity}
+                onMouseover={() => this.onMouseoverCity(index)}
+                icon="https://www.holidayhouses.co.nz/ReactApp/images/search/house_marker.png"
+            />
         })
     }
 
@@ -60,8 +89,16 @@ class MapComponent extends Component {
                             style={mapStyles}
                             initialCenter={{ lat: 11.127123, lng: 78.656891 }}
                             onDragend={this.centerMoved}
+                            onClick={this.onMapClicked}
                         >
                             {this.displayMarkers()}
+                            <InfoWindow
+                                marker={this.state.activeStore}
+                                visible={this.state.showingInfoWindow}>
+                                <div>
+                                    <HouseCard />
+                                </div>
+                            </InfoWindow>
                         </Map>
                     </div>
                 </div>
