29305244971ae164485ad58dac5af424e42f02bf
millenniumapps2020 <millenniumapps2020@gmail.com>
2020-05-29 19:51:45 +0530

    BookingPaymemt Details  - Intermediate Commit

--- src/screens/bookingpaymentdetails/bookingpymntdtls.js
+++ src/screens/bookingpaymentdetails/bookingpymntdtls.js
@@ -27,7 +27,23 @@ const languages = [
 
 class BookingPaymentDetails extends Component {
     state = {
-        selectedHouseName : "Sample House"
+        selectedHouseName : "Sample House",
+        bookingDetails : {
+            checkInDate : "17 Jun",
+            checkOutDate  : "20 Jun",
+            Guests : "1 Adult",
+            stayDays : '2nights@$110',
+            stayPrice : '$220',
+            extraCharges: [{
+                serviceName : 'Cleaning Fee',
+                serviceCharge : '$90'
+            }],
+            totalPrice : '$310.00 '
+        },
+        ownersPolicy : [{
+            policyName : 'Cancellation Policy',
+            policyDetails : ""
+        }]
     }
 
     render() {
@@ -47,45 +63,145 @@ class BookingPaymentDetails extends Component {
                                 <h6 className="RequestFont">Request Booking For {this.state.selectedHouseName}</h6>
                                 <p className="NoteText">Your booking will be processed through our secure platform. To keep your information safe, always use Holiday Houses to pay.</p>
                                 <div className="DetailsSectionBox row">
-                                    <div className="UserDetailsBox col-md-6">
-                                        <i class="fa fa-comment-o green lg"></i> <h4  className="">Your Details</h4>
-                                        <form>
-                                            <div className="row form-inline">
-                                                <div className="col form-group">
-                                                    <label>Name: </label>
-                                                    <input type="email"  id="email" />
-                                                    <div className="col">
-                                                        <button type="button" class="btn btn-link">Edit</button>
+                                    <div className="col-md-6">
+                                        <div className="UserDetailsBox">
+                                            <div className="container">
+                                                <i class="fa fa-comment-o green lg"></i> <h4  className="">Your Details</h4>
+                                                <form className="form-horizontal">
+                                                    <div className="form-inline">
+                                                        <div className="form-group">
+                                                            <label className="control-label col-sm-2">Name:</label>
+                                                            <div className="col-sm-10">
+                                                                <input type="text"  class="form-control" maxLength="50"/>
+                                                            </div>
+                                                        </div>
+                                                        <a className="EditButton">Edit</a>
                                                     </div>
-                                                </div> 
-                                                <div className="col form-group">
-                                                    <label>Email: </label>
-                                                    <input type="email"  id="email" />
-                                                    <div className="col">
-                                                        <button type="button" class="btn btn-link">Edit</button>
+                                                    <div className="form-inline form-Input">
+                                                        <div className="form-group">
+                                                            <label className="control-label col-sm-2">Email:</label>
+                                                            <div className="col-sm-10">
+                                                                <input type="email"  class="form-control" maxLength="50"/>
+                                                            </div>
+                                                        </div>
+                                                        <a className="EditButton">Edit</a>
                                                     </div>
-                                                </div> 
-                                                <div className="col form-group">
-                                                    <label>Phone: </label>
-                                                    <input type="email"  id="email" />
-                                                    <div className="col">
-                                                        <button type="button" class="btn btn-link">Edit</button>
+                                                    <div className="form-inline form-Input">
+                                                        <div className="form-group">
+                                                            <label className="control-label col-sm-2">Phone:</label>
+                                                            <div className="col-sm-10">
+                                                                <input type="number"  class="form-control" maxLength="50"/>
+                                                            </div>
+                                                        </div>
+                                                        <a className="EditButton">Edit</a>
                                                     </div>
-                                                </div> 
+                                                    <div className="border"></div>
+                                                    <div className="textField">
+                                                    <label>Your Message: </label>
+                                                        <textarea class="form-control" rows="3"></textarea>
+                                                    </div>
+                                                    <div className="backGroundText">
+                                                        <p className="NoteText">Have a question before you book? <a>Message the owner</a></p>
+                                                    </div>
+                                                </form>
                                             </div>
-                                            <div className="border"></div>
-                                            <div>
-                                            <label>Your Message: </label>
-                                                   <textarea class="form-control" rows="3"></textarea>
+                                        </div>
+                                        <div className="OwnerPolicySection">
+                                            <div className="container">
+                                              <h4  className="">Owner's Policy</h4>
+                                              <div className="PolicyDetails">
+                                                 <h6  className="">Cancellation Policy</h6>
+                                                 <p>If a cancellation is made within 7 days of check-in, you'll lose 100% of the total booking price.</p>
+                                                 <p>A booking service charge of $75 applies to all cancellation refunds.</p>
+                                              </div>
                                             </div>
-                                            <div className="backGroundText">
-                                                <p className="NoteText">Have a question before you book? <a>Message the owner</a></p>
+                                        </div>
+                                        <div className="Agreement">
+                                            <div className="custom-control custom-checkbox">
+                                                <input type="checkbox" className="custom-control-input"/>
+                                                <label className="custom-control-label">I agree to the cancellation policy and terms and conditions</label>
                                             </div>
-                                        </form>
+                                        </div>
+                                        <div className = "buttonDiv">
+                                        <button type="button" class="btn btn-warning btn-block" onClick={() => this.validateAndDoNextStep()}>Continue to Payment</button>
+                                        </div>
+                                        <div className = "crediText">
+                                            <p>Your credit card will not be charged unless the booking is confirmed.</p>
+                                        </div>
                                     </div>
                                     <div className="BookingDetailsBox col-md-6">
                                         <div className="container BookingInnerbox">
                                             <h6 className="DetailNote">Booking Details</h6>
+                                            <div className="row container">
+                                                <div className="CheckInDetails">
+                                                    <h6>Check in</h6>
+                                                    <p>{this.state.bookingDetails.checkInDate}</p>
+                                                </div>
+                                                <div className="CheckOutDetails">
+                                                    <h6>Check out</h6>
+                                                    <p>{this.state.bookingDetails.checkOutDate}</p>
+                                                </div>
+                                            </div>
+                                            <div className="GuestDetails">
+                                                <h6>Guests</h6>
+                                                <p>{this.state.bookingDetails.Guests}</p>
+                                            </div>
+                                            <div className="border-dottted"></div>
+                                            <div className="row container">
+                                                <div className="stayDetails">
+                                                    <p>{this.state.bookingDetails.stayDays}</p>
+                                                </div>
+                                                <div className="RateDetails">
+                                                    <p>{this.state.bookingDetails.stayPrice}</p>
+                                                </div>
+                                            </div>
+                                            <div className="border-dottted"></div>
+                                            <div className="row container">
+                                                {this.state.bookingDetails.extraCharges.map(item => {
+                                                    return (
+                                                        <div className="stayDetails">
+                                                            <p>{item.serviceName}</p>
+                                                        </div>
+                                                    )
+                                                   
+                                                 })}
+                                                 {this.state.bookingDetails.extraCharges.map(item => {
+                                                     return (
+                                                        <div className="RateDetails">
+                                                            <p>{item.serviceCharge}</p>
+                                                        </div>
+                                                    )
+                                                 })}
+                                                
+                                            </div>
+                                            <div className="border"></div>
+                                            <div className="row container">
+                                                <div className="stayDetails">
+                                                    <p>Total</p>
+                                                </div>
+                                                <div className="RateDetails">
+                                                    <span className="Rate">{this.state.bookingDetails.totalPrice}</span><span>NZD</span>
+                                                </div>
+                                            </div>
+                                            <div className="NotedText">
+                                                <p>
+                                                This payment will be charged by Holiday Houses on behalf of the property owner.
+                                                </p>
+                                                <p>
+                                                Payments will be held in a secure trust account by Holiday Houses until after you have checked in.
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <div className="bookimgSteps">
+                                            <h6 className="DetailNote">How to book this house : </h6> 
+                                            <div>
+                                                <ol>
+                                                    <li className="listtext">Fill out the attached form to get started with your booking.</li>
+                                                    <li className="listtext">Read and agree to the owner’s policies and Holiday Houses terms and conditions.</li>
+                                                    <li className="listtext">Complete the payment process - Your credit card will not be charged until the booking is confirmed.</li>
+                                                    <li className="listtext">This booking is subject to confirmation by the owner, who will confirm or decline your booking within 36 hours. Booking details will be emailed to you along with a reference.</li>
+                                                </ol>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
